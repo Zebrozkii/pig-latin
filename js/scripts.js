40ce694750b5f51bc1348e89baf3bd0ae107c6ed
Jared Hanson and Jimmy Zebroski <jared@>
2019-01-24 15:02:15 -0800

    add test variables for indexing features

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,15 @@ $(document).ready(function() {
 
     var vowel  = ['a','e','i','o','u'];
     var words = $("#input").val().split("");
+    var wordLength = words.length;
     var result = [];
+    var numb = 0;
+    var test = 2;
+    alert(words[test]);
 
+for (y = 0; y < wordLength; y++) {
+
+}
 
     for (x = 0; x < vowel.length; x++) {
       if (result == false) {
@@ -13,21 +20,22 @@ $(document).ready(function() {
           result = words;
           result.push("way");
         }
-        else if (words[1] === vowel[x]) {
+        else if (words[y] === vowel[x]) {
           words.push(words[0]);
           words.shift();
           result = words;
           result.push("ay");
         }
-        else if (words[2] === vowel[x]) {
-          words.push(words[0]);
-          words.push(words[0]);
-          words.shift();
-          words.shift();
-          result = words;
-          result.push("ay");
-        }
+        // else if (words[2] === vowel[x]) {
+        //   words.push(words[0]);
+        //   words.push(words[0]);
+        //   words.shift();
+        //   words.shift();
+        //   result = words;
+        //   result.push("ay");
+        // }
       }
+      numb++
     }
 
     // alert(result);
